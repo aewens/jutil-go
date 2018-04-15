5363672e75131195d203a0b3ce12dcbf5e0122c2
aewens <email@aewens.com>
2018-04-15 18:00:57 -0500

    Fixed bug, flags working

--- jutil.go
+++ jutil.go
@@ -20,6 +20,7 @@ func main() {
     // [ ]: -j (json)
     // [ ]: -v (value)
     
+    // Make sure arguments were passed
     if len(os.Args[1:]) == 0 {
         defer func() {
             if r := recover(); r != nil {
@@ -29,9 +30,7 @@ func main() {
         panic("[0x01]: No arguments passed")
     }
     
-    // flag.String(param, default, description)
-    // flag.StringVar(pointer*, param, default, description)
-    
+    // Define flag value variables
     var fileAction string
     var readAction string
     var editAction string
@@ -42,15 +41,33 @@ func main() {
     var jsonAction string
     var valueAction string
     
-    filePtr := flag.StringVar(&fileAction, "-f", "", "Path to JSON file")
-    readPtr := flag.StringVar(&readAction, "-r", "", "Path to read")
-    editPtr := flag.StringVar(&editAction, "-e", "", "Path to edit")
-    appendPtr := flag.StringVar(&appendAction, "-a", "", "Path to append to")
-    deletePtr := flag.StringVar(&deleteAction, "-d", "", "Path to delete")
-    iniPtr := flag.StringVar(&iniAction, "-i", "", "INI output file name")
-    createPtr := flag.StringVar(&createAction, "-c", "", "File to create from")
-    jsonPtr := flag.StringVar(&jsonAction, "-j", "", "JSON output file name")
-    valuePtr := flag.StringVar(&valueAction, "-v", "", "Value to use")
+    // NOTE
+    // flag.String(param, default, description)
+    // flag.StringVar(pointer*, param, default, description)
     
+    // Flag defitions
+    flag.StringVar(&fileAction, "f", "", "Path to JSON file")
+    flag.StringVar(&readAction, "r", "", "Path to read")
+    flag.StringVar(&editAction, "e", "", "Path to edit")
+    flag.StringVar(&appendAction, "a", "", "Path to append to")
+    flag.StringVar(&deleteAction, "d", "", "Path to delete")
+    flag.StringVar(&iniAction, "i", "", "INI output file name")
+    flag.StringVar(&createAction, "c", "", "File to create from")
+    flag.StringVar(&jsonAction, "j", "", "JSON output file name")
+    flag.StringVar(&valueAction, "v", "", "Value to use")
     
+    // Parse all flags
+    flag.Parse()
+    
+    // Get output
+    fmt.Println("fileAction:", fileAction)
+    fmt.Println("readAction:", readAction)
+    fmt.Println("editAction:", editAction)
+    fmt.Println("appendAction:", appendAction)
+    fmt.Println("deleteAction:", deleteAction)
+    fmt.Println("iniAction:", iniAction)
+    fmt.Println("createAction:", createAction)
+    fmt.Println("jsonAction:", jsonAction)
+    fmt.Println("valueAction:", valueAction)
+    fmt.Println("remainder:", flag.Args())
 }
