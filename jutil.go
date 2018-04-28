f1ef7bdb0f7962d15e710a3796dcdb1fac140b39
aewens <email@aewens.com>
2018-04-27 19:41:03 -0500

    Optimized getContext to be more versitile

--- jutil.go
+++ jutil.go
@@ -4,6 +4,7 @@ import (
     "encoding/json"
     "path/filepath"
     "io/ioutil"
+    "strings"
     "flag"
     "fmt"
     "os"
@@ -11,7 +12,7 @@ import (
 
 func main() {
     // TODO
-    // [ ]: -f (file)
+    // [*]: -f (file)
     // [ ]: -r (read)
     // [ ]: -e (edit)
     // [ ]: -a (append)
@@ -21,6 +22,7 @@ func main() {
     // [ ]: -j (json)
     // [ ]: -v (value)
     
+    // Catch panics here
     defer func() {
         if r := recover(); r != nil {
             fmt.Println("Error", r)
@@ -62,44 +64,87 @@ func main() {
     flag.Parse()
     
     // Handle by context
-    switch getContext(fileAction, createAction) {
+    rootActions := make(map[string]bool)
+    rootActions["file"] = len(fileAction) > 0
+    rootActions["create"] = len(createAction) > 0
+    
+    switch getContext(rootActions) {
     case "file":
-        var fileMode Mode = createMode("file", fileAction)
-        var contents map[string]interface{} = fileMode.getContents()
+        // var fileMode Mode = createMode("file", fileAction)
+        // var contents map[string]interface{} = fileMode.getContents()
         
-        // TODO: Remove prints, move into function, add map handler / recursion
-        for key, val := range contents {
-            switch value := val.(type) {
-            case []interface{}:
-                fmt.Println(key, fmt.Sprintf("json[%d]", len(value)))
-                for i, v := range value {
-                    fmt.Println(i, v)
-                }
-            default:
-                fmt.Println(key, value)
-            }
+        subActions := make(map[string]bool)
+        subActions["read"] = len(readAction) > 0
+        subActions["edit"] = len(editAction) > 0
+        subActions["append"] = len(appendAction) > 0
+        subActions["delete"] = len(deleteAction) > 0
+        
+        switch getContext(subActions) {
+        case "read":
+            fmt.Println("Read is coming soon!")
+        case "edit":
+            fmt.Println("Edit is coming soon!")
+        case "append":
+            fmt.Println("Append is coming soon!")
+        case "delete":
+            fmt.Println("Delete is coming soon!")
         }
+        
+        // // TODO: Remove prints, move into function, add map handler / recursion
+        // for key, val := range contents {
+        //     switch value := val.(type) {
+        //     case []interface{}:
+        //         fmt.Println(key, fmt.Sprintf("json[%d]", len(value)))
+        //         for i, v := range value {
+        //             fmt.Println(i, v)
+        //         }
+        //     default:
+        //         fmt.Println(key, value)
+        //     }
+        // }
         
         fmt.Println("Success [0x00]")
     case "create":
-        fmt.Println("Coming soon!")
+        fmt.Println("Create is coming soon!")
     }
 }
 
-// Determine context of script
-func getContext(fileAction, createAction string) string {
-    var fileAssigned bool = len(fileAction) > 0
-    var createAssigned bool = len(createAction) > 0
-    
-    if fileAssigned && createAssigned {
-        panic("[0x02]: -f and -c cannot be used together")
-    } else if fileAssigned {
-        return "file"
-    } else if createAssigned {
-        return "create"
-    } else {
-        panic("[0x03]: Neither -f nor -c was selected")
+// Count how many bools are true
+func countBool(tests map[string]bool) (int, []string) {
+    var count int = 0
+    var names []string
+    for name, test := range tests {
+        if test {
+            count = count + 1
+            names = append(names, name)
+        }
     }
+    return count, names
+}
+
+// Determine context of script
+func getContext(rootActions map[string]bool) string {
+    truthCount, trueNames := countBool(rootActions)
+    
+    if (truthCount > 1) {
+        allFlags := make([]string, len(trueNames))
+        for n, name := range trueNames {
+            allFlags[n] = fmt.Sprintf("-%s", name[0])
+        }
+        
+        var andFlags string = strings.Join(allFlags, " and ")
+        panic(fmt.Sprintf("[0x02]: %s cannot be used together", andFlags))
+    } else if (truthCount == 0) {
+        allFlags := make([]string, len(trueNames))
+        for n, name := range trueNames {
+            allFlags[n] = fmt.Sprintf("-%s", name[0])
+        }
+        
+        var norFlags string = strings.Join(allFlags, " nor ")
+        panic(fmt.Sprintf("[0x03]: Neither %s was selected", norFlags))
+    }
+    
+    return trueNames[0]
 }
 
 type Mode struct {
